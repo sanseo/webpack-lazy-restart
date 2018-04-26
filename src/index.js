0414cc52f7fa1b234de0aaa88fb78fe060bb21d2
senseo <328966894@qq.com>
2018-04-26 18:05:54 +0800

    manage output

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
+// import './style.css';
+// import  Icon from './refresh.png';
+// import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div');
+    var btn =document.createElement('button');
+
     //lodash ,now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], '');
     element.classList.add('hello')
+
+    btn.innerHTML='Click me and check the console';
+    btn.onclick=printMe;
+
+    element.appendChild(btn);
+    // var myIcon=new Image();
+    // myIcon.src=Icon;
+    //
+    // element.appendChild(myIcon);
+    //
+    // console.log(Data);
+
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,47 @@
 const path =require('path');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        app:"./src/index.js",
+        print:'./src/print.js'
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname,'dist')
     },
-    module : {
-        rules : [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    }
+    // module : {
+    //     rules : [
+    //         {
+    //             test: /\.css$/,
+    //             use: [
+    //                 'style-loader',
+    //                 'css-loader'
+    //             ]
+    //         },
+    //         {
+    //             test:/\.(png|svg|jpg|gif)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test:/\.(csv|tsv)$/,
+    //             use:[
+    //                 'csv-loader'
+    //             ]
+    //         },
+    //         {
+    //             test:/\.xml$/,
+    //             use:[
+    //                 'xml-loader'
+    //             ]
+    //         }
+    //     ]
+    // }
 }
