493493383c8d525d01134c3c45700cb47b7d061a
senseo <328966894@qq.com>
2018-04-28 15:55:23 +0800

    manage output

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path =require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin =require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -9,6 +11,12 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname,'dist')
     },
+    plugins:[
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+          title:'Output Management'
+      })
+    ],
     // module : {
     //     rules : [
     //         {
